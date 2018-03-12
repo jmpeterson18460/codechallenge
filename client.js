c5dcd6acd3b0bc3a67e9ac566ffb0b1fe572ac37
Justin <jmpeterson9891@gmail.com>
2018-03-12 14:30:21 -0500

    Got some of it :P

--- client.js
+++ client.js
@@ -10,14 +10,19 @@ function readyNow(){
 function addDom (){
   counter ++;
   let theDivider = $('<div><p>Number of times clicked: </div>');
-  $('.content').append(theDivider);
+  $('.bling').append(theDivider);
   theDivider.append(counter);
-  //createPar();
+  theDivider.append('<button id="changeo">Swap</button>');
+  theDivider.append('<button id="garbage">Delete</button>');
+  $('#changeo').on('click', changeColor);
+  $('.bling').on('click', '#garbage', removeDiv);
 }
 
-//function createPar (){
-  //$('.money').empty();
-  //let dollars = $('<p>Number of times clicked: </p>');
-  //$('.money').append(dollars);
-  //dollars.append(counter);
-//}
+function changeColor (){
+  $('.bling').css('background-color', 'yellow');
+}
+
+function removeDiv (){
+  let x = $(this).parent().parent();
+  x.empty();
+}
