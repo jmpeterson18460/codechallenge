0fe1c289d9b615f85a8a1b64523c1c47185a9363
Justin <jmpeterson9891@gmail.com>
2018-03-12 13:41:37 -0500

    Added counter for button

--- client.js
+++ client.js
@@ -1,10 +1,22 @@
 $(document).ready(readyNow);
 
+let counter = 0;
+
 function readyNow(){
+
   $('#swagger').on('click', addDom);
 }
 
 function addDom (){
+  counter ++;
   let theDivider = $('<div class="money"></div>');
   $('.content').append(theDivider);
+  createPar();
+}
+
+function createPar (){
+  $('.money').empty();
+  let dollars = $('<p>Number of times clicked: </p>');
+  $('.money').append(dollars);
+  dollars.append(counter);
 }
